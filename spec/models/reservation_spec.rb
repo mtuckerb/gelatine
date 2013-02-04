ce9198d388b265bcf77d742e8b7996dbc36c5a10
Tucker bradford <tucker@tuckerbradford.com>
2013-02-04 21:25:13 +1000

    changed operating days start/stop to operating days serialized

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'chronic'
 
 describe Reservation do
   before(:each) do
@@ -8,5 +9,9 @@ describe Reservation do
   it "should be invlaid without a user id" do
     @reservation.should have(1).error_on(:user_id)
     @reservation.should_not be_valid
+  end
+  
+  context "room not available that day" do
+    Chronic.@reservation.opstartday
   end
 end
