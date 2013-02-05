11a8024ae9faa82f3999cbeac17a92634abe8fc0
Tucker bradford <tucker@tuckerbradford.com>
2013-02-06 07:05:24 +1000

    Add name to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,21 @@
 
 FactoryGirl.define do
   factory :user do
+    name Faker::Name.name
+    sequence :email do |n|
+      email = "person#{n}@#{Faker::Internet.domain_name}"
+    end
+    password "This15apassword"
+    password_confirmation "This15apassword"
+  end
+  
+  factory :user_with_reservations do
+    ignore do
+      reservation_count 5
+    end
+
+    after(:create) do |user, evaluator|
+      FactoryGirl.create_list(:reservation, evaluator.reservations_count, user: user)
+    end
   end
 end
