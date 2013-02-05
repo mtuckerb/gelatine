4165c68226f49c9b9e0737aebd3119154855730c
Tucker bradford <tucker@tuckerbradford.com>
2013-02-06 07:51:48 +1000

    added name to User (part 2)

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :room do |f|
-    f.name  Faker::Name.name 
+    f.sequence(:name) {|n| Faker::Name.name }
     f.ophoursstart "0900"
     f.ophoursstop "1700"
     f.operating_days 'Mon Tue Fri'
