f821abbc83e25c2f0865cf1607545ea945bb83e7
Tucker bradford <tucker@tuckerbradford.com>
2013-02-05 14:08:25 +1000

    created basic FactoryGirl for reservations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ActiveRecord::Base
   attr_accessible :capacity, :name, :notes, :ophoursstart, :ophoursstop, :operating_days
-  #validates_presence_of :name, :capacity, :opdaystart, :opdaystop, :ophoursstart, :ophoursstop
+  validates_presence_of :name, :capacity, :operating_days, :ophoursstart, :ophoursstop
 end
