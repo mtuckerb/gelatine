8715a8059ed9686bed1c7459c18a9c8c31b5acea
Tucker bradford <tucker@tuckerbradford.com>
2013-02-05 16:33:34 +1000

    Finish the Devise config

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ActiveRecord::Base
   belongs_to :room
+  belongs_to :user
   attr_accessible :attendie_count, :room_id, :start_time, :stop_time, :user_id
   validates_presence_of :user_id
   serialize :operating_days
