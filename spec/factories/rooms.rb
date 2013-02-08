53b6d56e8014b7543af44587e236204b71af84d2
Tucker bradford <tucker@tuckerbradford.com>
2013-02-08 16:19:14 +1000

    Added booking hours validation and changed Factory so it wouldn't break every other test. fixed unit test for room booked set default timezones

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     f.sequence(:name) {|n| Faker::Name.name }
     f.ophoursstart "0900"
     f.ophoursstop "1700"
-    f.operating_days 'Mon Tue Fri'
+    f.operating_days 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
     f.capacity '2'
     f.notes Faker::Lorem.sentence
   end
