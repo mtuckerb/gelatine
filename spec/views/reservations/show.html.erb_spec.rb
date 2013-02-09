857ac671920f22f32d8190ee68711174bb47a73b
Tucker bradford <tucker@tuckerbradford.com>
2013-02-09 13:51:32 +1000

    refactored view tests. all green

--- spec/views/reservations/show.html.erb_spec.rb
+++ spec/views/reservations/show.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "reservations/show" do
   before(:each) do
     @reservation = assign(:reservation, stub_model(Reservation,
-      :user_id => 1,
-      :room_id => 2,
+    :room => stub_model(Room, :id => 2, :name => "Window Bay 7"),
+    :user => stub_model(User, :id => 1, :name => "Tucker"),
       :attendie_count => 3
     ))
   end
@@ -12,8 +12,8 @@ describe "reservations/show" do
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    rendered.should match(/2/)
+    rendered.should match(/Tucker/)
+    rendered.should match(/Window Bay 7/)
     rendered.should match(/3/)
   end
 end
