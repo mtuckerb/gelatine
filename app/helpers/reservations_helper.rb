4439ec0259b4f7f3d91058d641810f7ba0aff65f
Tucker bradford <tucker@tuckerbradford.com>
2013-02-10 20:44:49 +1000

    cleaned up time/dates in  view a little.

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,9 @@
 module ReservationsHelper
+  
+  def format_time(start_time,stop_time)
+    return start_time.strftime("%l:%M%P ") + " - " + stop_time.strftime("%l:%M%P")
+  end
+  def format_date(stop_date)
+    return " "+ stop_date.strftime("%b #{stop_date.day.ordinalize} %Y")
+  end
 end
