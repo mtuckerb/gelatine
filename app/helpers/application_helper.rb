1bfed0d00ea909779938d86b58f0c6261335f272
Tucker bradford <tucker@tuckerbradford.com>
2013-03-10 19:27:53 +1000

    added monsterid as default avatar type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   	if profile.avatar_image
   		return(image_tag profile.avatar_image.thumb("100x100#").url :class => "thumbnail media border-img")
   	else
-  		gravatar_image_tag(profile.user.email, :alt => profile.user.name)
+  		gravatar_image_tag(profile.user.email, :alt => profile.user.name, :gravatar => {:default => :monsterid})
 	end
   end
 
